807051312f95a4463058c784bdf11847b60efe98
dexter <dexter@newlogic.io>
2020-01-06 19:28:33 +0800

    Save 3 bytes in applet

--- src/main/java/org/idpass/tools/IdpassApplet.java
+++ src/main/java/org/idpass/tools/IdpassApplet.java
@@ -52,8 +52,6 @@ public abstract class IdpassApplet extends Applet implements ExtendedLength, App
     protected byte              p2;
 
     protected SecureChannel       secureChannel;
-    protected short aid_offset;
-    protected byte aid_len;
 
     public boolean select() {
         // retrieve the handle of the Security Domain associated with this
